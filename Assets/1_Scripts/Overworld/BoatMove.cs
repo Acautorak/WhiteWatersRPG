8af99481c6d0dde73a7094e5266ff873cf7c9cde
Utorak <acislav@gmail.com>
2023-12-07 14:55:55 +0100

    boat move
    priprema za roguelike world

---
+++ Assets/1_Scripts/Overworld/BoatMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMove : MonoBehaviour
+{
+    [SerializeField] private Transform[] path0, path1, path2;
+    [SerializeField] private int pathIndex = 0;
+    [SerializeField] private float moveTime = 2f, rotationTime = 5f;
+    [SerializeField] private GameObject boat;
+
+    private void Start()
+    {
+        StartMovement();
+    }
+    public void StartMovement()
+    {
+        MoveToNextWaypoint(path0);
+    }
+
+    private void MoveToNextWaypoint(Transform[] path)
+    {
+        if (pathIndex < path.Length)
+        {
+            Vector2 targetPosition = path[pathIndex].transform.position;
+            LeanTween.move(boat, targetPosition, moveTime).setEase(LeanTweenType.linear).setOnComplete(() => { pathIndex++; MoveToNextWaypoint(path); });
+            LeanTween.rotateZ(boat, CalculateTargetRotation(targetPosition), rotationTime).setEase(LeanTweenType.easeInOutQuad);
+        }
+        else
+        {
+            pathIndex = 0;
+            Debug.LogWarning("dosli smo do kraja");
+        }
+    }
+
+    private float CalculateTargetRotation(Vector2 targetPosition)
+    {
+        Vector2 direction = (targetPosition - (Vector2)boat.transform.position).normalized;
+        return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
+    }
+}
