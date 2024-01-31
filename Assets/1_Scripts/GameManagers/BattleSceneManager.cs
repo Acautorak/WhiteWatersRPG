db98f8dfaaffff8f0d0a893e0b9e0bf39423cee1
Utorak <acislav@gmail.com>
2024-01-31 17:35:40 +0100

    AAAAAAAAA
    Shitty Async Asset load failed...
    TOmorrows gonna be fking insane bruh

--- Assets/1_Scripts/GameManagers/BattleSceneManager.cs
+++ Assets/1_Scripts/GameManagers/BattleSceneManager.cs
@@ -9,6 +9,11 @@ public class BattleSceneManager : MonoBehaviour
 
     public GridPosition gridPosition1, gridPosition2, gridPosition3, gridPosition4;
 
+    private List<GridPosition> enemyGridPositionList;
+
+    public string unitPrefabsAddressableKey = "UnitPrefabs";
+    public int numberOfUnitsToInstantiate = 4;
+
     private void Awake()
     {
         if (Instance != null)
@@ -20,11 +25,69 @@ public class BattleSceneManager : MonoBehaviour
         Instance = this;
     }
 
-    public void SetupParty()
+    private void Start()
     {
-        foreach (PartyUnit partyUnit in PartyManager.Instance.GetPartyUnitList())
+        gridPosition1 = new GridPosition(5 - 1, 1);
+        gridPosition2 = new GridPosition(5 - 1, 3);
+        gridPosition3 = new GridPosition(5 - 1, 4);
+        gridPosition4 = new GridPosition(5 - 1, 6);
+
+        enemyGridPositionList = new List<GridPosition>
         {
-            
+            gridPosition1, gridPosition2, gridPosition3, gridPosition4
+        };
+
+        LoadUnitPrefabs();
+    }
+
+    void LoadUnitPrefabs()
+    {
+        AsyncOperationHandle<IList<GameObject>> handle = Addressables.LoadAssetsAsync<GameObject>(unitPrefabsAddressableKey, null);
+
+        handle.Completed += OnUnitPrefabsLoaded;
+    }
+
+    void OnUnitPrefabsLoaded(AsyncOperationHandle<IList<GameObject>> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            IList<GameObject> unitPrefabs = handle.Result;
+
+            // Shuffle unitPrefabs if needed
+            ShuffleUnitPrefabs(unitPrefabs);
+
+            // Instantiate the first numberOfUnitsToInstantiate units
+            for (int i = 0; i < Mathf.Min(numberOfUnitsToInstantiate, unitPrefabs.Count); i++)
+            {
+                Instantiate(unitPrefabs[i], LevelGrid.Instance.GetWorldPosition(enemyGridPositionList[i]), Quaternion.identity);
+            }
+
+            // Unload unit prefabs when they are no longer needed
+            UnloadUnitPrefabs(unitPrefabs);
+        }
+        else
+        {
+            Debug.LogError($"Failed to load unit prefabs: {handle.Status}");
+        }
+    }
+
+    void ShuffleUnitPrefabs(IList<GameObject> unitPrefabs)
+    {
+        // Shuffle unitPrefabs using Fisher-Yates algorithm
+        for (int i = unitPrefabs.Count - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            GameObject temp = unitPrefabs[i];
+            unitPrefabs[i] = unitPrefabs[randomIndex];
+            unitPrefabs[randomIndex] = temp;
+        }
+    }
+
+    void UnloadUnitPrefabs(IList<GameObject> unitPrefabs)
+    {
+        foreach (GameObject prefab in unitPrefabs)
+        {
+            Addressables.ReleaseInstance(prefab);
         }
     }
 
