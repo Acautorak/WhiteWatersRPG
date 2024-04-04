9f3b157110a7161d0195ba238f06c6e712df5380
Utorak <acislav@gmail.com>
2024-04-04 11:04:43 +0200

    Gotovo peglanje sa UI
    ili ja barem tako mislim

--- Assets/1_Scripts/UX/ConsumablesMangerUi.cs
+++ Assets/1_Scripts/UX/ConsumablesMangerUi.cs
@@ -7,12 +7,20 @@ public class ConsumablesMangerUi : MonoBehaviour
 {
     [SerializeField] private Button consumableButton, arrowButton, swap1, swap2, swap3, swap4;
     [SerializeField] private Sprite arrowSprite, reverseArrowSprite;
+    [SerializeField] private GameObject swapContainer;
+
+    private int currentSelectedConsumable;
 
     private bool arrowUP = true;
 
     private void Start()
     {
         arrowButton.onClick.AddListener(() => OnArrowClick());
+        consumableButton.onClick.AddListener(() => OnConsumeClicked());
+        swap1.onClick.AddListener(() => OnSwapClick1());
+        swap2.onClick.AddListener(() => OnSwapClick2());
+        swap3.onClick.AddListener(() => OnSwapClick3());
+        swap4.onClick.AddListener(() => OnSwapClick4());
     }
 
     public void OnArrowClick()
@@ -21,15 +29,44 @@ public class ConsumablesMangerUi : MonoBehaviour
         if (arrowUP)
         {
             arrowButton.image.sprite = arrowSprite;
+            swapContainer.SetActive(false);
         }
 
         //Open
         if (!arrowUP)
         {
             arrowButton.image.sprite = reverseArrowSprite;
+            swapContainer.SetActive(true);
         }
 
         arrowUP = !arrowUP;
 
+    }
+
+    public void OnConsumeClicked()
+    {
+        //Consume equippedConsumable[currentSelectedConsumable]
+        Debug.Log("Trenutno je " + currentSelectedConsumable);
+
+    }
+    public void OnSwapClick1()
+    {
+        consumableButton.image.sprite = swap1.image.sprite;
+        currentSelectedConsumable = 0;
+    }
+    public void OnSwapClick2()
+    {
+        consumableButton.image.sprite = swap2.image.sprite;
+        currentSelectedConsumable = 1;
+    }
+    public void OnSwapClick3()
+    {
+        consumableButton.image.sprite = swap3.image.sprite;
+        currentSelectedConsumable = 2;
+    }
+    public void OnSwapClick4()
+    {
+        consumableButton.image.sprite = swap4.image.sprite;
+        currentSelectedConsumable = 3;
     }
 }
