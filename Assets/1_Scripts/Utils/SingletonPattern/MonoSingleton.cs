cccb6bdc86ac73fa1b05fc6b5698d74dd8eb6dea
Djordje Cvijic <djordjec@twodesperados.com>
2024-05-06 19:40:17 +0200

    add cool utils

---
+++ Assets/1_Scripts/Utils/SingletonPattern/MonoSingleton.cs
@@ -0,0 +1,103 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    // ReSharper disable once StaticMemberInGenericType
+    private static readonly object Lock = new();
+
+    // ReSharper disable once StaticMemberInGenericType
+    private static bool applicationIsQuitting;
+
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (applicationIsQuitting)
+            {
+                return null;
+            }
+
+            lock (Lock)
+            {
+                if (instance == null)
+                {
+                    instance = GetOrCreateInstance();
+                    if (instance != null) instance.ApplyPersistenceIfNeeded();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    private static T GetOrCreateInstance()
+    {
+        var instances = FindObjectsOfType<T>();
+        if (instances.Length == 0)
+        {
+            return typeof(ISelfInstantiatingMonoSingleton).IsAssignableFrom(typeof(T))
+                ? new GameObject($"{typeof(T).Name}").AddComponent<T>()
+                : null;
+        }
+        else if (instances.Length == 1)
+        {
+            return instances[0];
+        }
+        else
+        {
+            for (var i = 1; i < instances.Length; i++)
+            {
+                Destroy(instances[i].gameObject);
+            }
+
+            return instances[0];
+        }
+    }
+
+    private void ApplyPersistenceIfNeeded()
+    {
+        if (!typeof(IPersistentMonoSingleton).IsAssignableFrom(typeof(T))) return;
+#if UNITY_EDITOR
+        if (transform.parent != null)
+        {
+            Debug.LogWarning("MonoSingleton must be a root game object so DontDestroyOnLoad can be applied!");
+            Selection.activeGameObject = gameObject;
+            Debug.Break();
+            return;
+        }
+#endif
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            if (typeof(IMonoSingletonPreferNewInstance).IsAssignableFrom(typeof(T)))
+            {
+                Destroy(instance.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+
+        ApplyPersistenceIfNeeded();
+        OnAwake();
+    }
+
+    protected virtual void OnAwake()
+    {
+    }
+
+    private void OnApplicationQuit()
+    {
+        applicationIsQuitting = true;
+    }
+}
