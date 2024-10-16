a3f98ace5630ea595cfbe03db0817422d29102ae
Utorak <acislav@gmail.com>
2024-10-16 17:23:21 +0200

    Friends update 3

--- Assets/1_Scripts/Utils/TestSolve.cs
+++ Assets/1_Scripts/Utils/TestSolve.cs
@@ -1,100 +1,61 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class TestSolve : MonoBehaviour
 {
-    public struct Friend
-    {
-        public int arrival;
-        public int leaving;
-        public int chair;
 
-        public Friend(int arrival, int leaving, int chair)
-        {
-            this.arrival = arrival;
-            this.leaving = leaving;
-            this.chair = chair;
-        }
+    ListNode CreateTestListWithCycle()
+    {
+        // Create nodes
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+
+        node1.next = node2;
+        node2.next = node1;
+
+
+
+        // Return the head of the list
+        return node1;
     }
 
-    public int [][] times = new int[][] {new int[] {1,2}, new int[] {2,3}};
-
-    public Dictionary<int, Friend> friendsMap = new Dictionary<int, Friend>();
-    public Queue<int> chairsQueue = new Queue<int>();
-
-    public int SmallestChair(int[][] times, int targetFriend)
+    public bool HasCycle(ListNode head)
     {
-        for (int i = 0; i < times.Length; i++)  
+        if(head == null || head.next == null)
+        return false;
+
+        ListNode pom1 = head;
+        ListNode pom2 = head.next;
+
+        while(pom2.next != null)
         {
-            for (int j = 0; j < times[i].Length; j++) 
+            pom1 = pom1.next;
+            pom2 = pom2.next.next;
+
+            if(pom1 == pom2)
             {
-               // Debug.Log($"Element at times[{i},{j}] = {times[i][j]}");
+                return true;
             }
         }
-        CreateMapFriends(friendsMap);
-        return 0;
+        return false;
     }
-
    
-
-    public int ReturnTargetChair(int targetFriend)
-    {
-        return friendsMap[targetFriend].chair;
-    }
-
-
-    public List<int> FindSmallestLargerTime(Dictionary<int, Friend> friends, int pomTime)
-    {
-        List<int> smallestLargerFriend = new List<int>(); 
-        int smallestTime = int.MaxValue;     
-
-        foreach (KeyValuePair<int, Friend> kvp in friends)
-        {
-            if (kvp.Value.arrival > pomTime && kvp.Value.arrival <= smallestTime)
-            {
-                smallestLargerFriend.Add(kvp.Key);
-                smallestTime = kvp.Value.arrival;
-            }
-
-            if (kvp.Value.leaving > pomTime && kvp.Value.leaving <= smallestTime)
-            {
-                smallestLargerFriend.Add(kvp.Key);
-                smallestTime = kvp.Value.leaving;
-            }
-        }
-
-        return smallestLargerFriend;
-    }
-
-   /*  public void ChangeChairsInMapFriends(List<int> smallestLargerFriend, int time)
-    {
-        int freeChair = 0;
-        int pomTime = ;
-
-        foreach (int element in smallestLargerFriend)
-        {
-            if(friendsMap(element).Value
-        }
-    } */
-
-
-    public void CreateMapFriends(Dictionary<int,Friend> friends)
-    {
-        int pom = 0;
-        foreach(int[] element in times)
-        {
-            friends.Add(pom, new Friend(element[0], element[1], -1));
-            Debug.Log($"Key: {pom}: Value {friends[pom].leaving}");
-            pom++;
-        }
-        
-    }
-
 
     void Start()
     {
-       SmallestChair(times, 1);
-       
+        Debug.Log($"{HasCycle(CreateTestListWithCycle())}");
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val)
+    {
+        this.val = val;
     }
 }
